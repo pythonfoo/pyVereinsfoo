06801f1ebf7e50cbb53c5972e655c795619e1f8b
Oerb <bjoern@oerb.de>
2013-05-12 14:44:33 +0200

    forgotten foo

---
+++ venlib/base/mandatory/address.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python 
+#-*- coding: utf-8 -*-
+
+""" 
+Data Main Address property based Dataclass. Adresses 
+are used everywhere in Project. so be Open in config
+think wise
+"""
+
+__authors__ = ("oerb")
+__copyright__ = "GPL 2013"
+__license__ = "GPL" 
+
+from venlib.base.venbase import DataObj
+
+class Address(DataObj):
+    """
+    contains the Address Propertys
+    """
+    pass
